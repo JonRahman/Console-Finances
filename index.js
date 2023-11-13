e88c933b6d86903df755c390d04260f35684740e
JonRahman <jonrahman90@gmail.com>
2023-11-13 16:52:11 +0000

    updated README

--- index.js
+++ index.js
@@ -130,7 +130,6 @@ for (var i = 1; i < finances.length; i++) {
   }
 }
 
-
 console.log("Financial Analysis");
 console.log("Total number of months: " + totalMonths + ", Total: $" + netTotal + ", Average Change: $" + averageChange.toFixed(2));
 console.log("Greatest Increase in Profits/Losses: $" + greatestIncrease.amount + " " + greatestIncrease.date + ", & Greatest Decrease in Profits/Losses: $" + greatestDecrease.amount + " " + greatestDecrease.date);
